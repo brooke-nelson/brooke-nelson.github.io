4d44bdad721099029cc9b541c3acdc2a5c96d220
brooke-nelson <59668833+brooke-nelson@users.noreply.github.com>
2020-06-03 17:27:38 -0700

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.css">
+<link rel="icon" href="images/favicon.ico">
+
+
+<title>Visit Bellingham</title>
+</head>
+<div id="wrapper">
+<body>
+<header>
+        <div class="container">
+            <div class="logo"><img src="images/bham_logo.png.PNG" alt=""></div>
+            <nav class="navigation">
+                <label class="label" for="toggle"><i class="fas fa-bars"></i></label>
+                <input type="checkbox" id="toggle">
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="explore.php">Explore</a>
+                        <ul class="dropdown">
+                            <li class="list"><a href="food.php">Food</a></li>
+                            <li class="list"><a href="events.php">Events</a></li>
+                            <li class="list"><a href="local.php">Local Treasures</a></li>
+                            <li class="list"><a href="spotlight.html">Weekly Spotlight</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="news.php">News</a></li>
+                    <li><a href="wwu.php">WWU</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="banner"></div>
+        </div>
+    </section>
+    <div class="clearfix"></div>
+<main>
+    <div class="container">  
+            <div class="main">
+                <div class="left">
+                    <h1 class="title">Explore the Great Pacific Northwest</h1>
+                    <p class="hometxt">Bellingham, Washington is located in Northwestern Washington, about 30 minutes from the Canadian border. The views, people, and food are some of the best you will find in Washington, perhaps the world. Explore our dozens of hiking, biking, and walking trails trails, or take a walk on the waterfront at one of our numerous waterfront parks. Relax on the beach or have a night out downtown, just remember to grab a bite to eat at any of our numerous local hot-spot restaurants. Let this website be your guide to experience the beautiful city of Bellingham!</p>
+                </div>
+
+                <div class="right">
+                    <h2 class="subhead">Weekly Spotlight</h2>
+                    <p class="hometxt">
+                        <img class="boomer" src="images/DSCN2170.jpg" alt=""><br> This weeks spotlight is the treasured Boomer's Drive In. Boomer's Drive In has been voted the best burger in town many times and provides a fun old-school theme. The unique burgers are good as any, plus the milkshakes and malts are unbeatable. <a href="spotlight.html">Read More...</a></p>
+                </div>
+            </div>
+            </div>
+        
+    <div class="clearfix"></div>
+
+</main>
+<div class="container">
+<div class="break"></div>
+</div>
+<footer>
+    <div class="container">
+        <div class="main-footer">
+            <div class="footer">
+                <h4 class="sub-heading">Follow Us!</h4>
+                    <p>Follow our Social Media accounts</p>
+                    <p><i class="fab fa-instagram"></i>     <i class="fab fa-facebook"></i>     <i class="fab fa-twitter-square"></i></p>
+            </div>
+            <div class="footer">
+                <h4 class="sub-heading">Contact Us!</h4>
+                <p>Have Questions or want to Add to our page? <br> <a href="contact.php">Click Here to Send Us a Message</a></p>
+            </div>
+        </div>
+            <div class="copyright"> &copy; Copyright 2019 All rights reserved | All photos in website are taken by owner | Brooke Nelson <br> 
+                <!-- <script>
+                    var date= document.lastModified;
+                    document.write(date);
+                </script> -->
+            </div>
+            <div class="clearfix"></div>
+        
+    </div>
+    
+</footer>
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.js"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.js "> </script>
+<script src="https://unpkg.com/scrollreveal"></script>
+
+<script>
+    //scroll reveal
+$('document').ready(function(){
+window.sr=ScrollReveal();
+
+sr.reveal('.main',{
+    duration:1000,
+    origin:'bottom',
+    delay:500,
+    distance:'100px'
+});
+sr.reveal('.subhead',{
+    duration:1000,
+    origin:'bottom',
+    delay:500,
+    distance:'100px'
+});
+});
+</script>
+
+
+</body>
+</div>
+</html>
